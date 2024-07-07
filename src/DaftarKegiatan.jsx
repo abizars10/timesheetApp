01e19f253482ca1b78125348a55392714df8c301
Abizar Ar Rifa'i <abizararrifai@gmail.com>
2024-07-07 12:39:48 +0700

    menampilkan data karyawan

--- src/DaftarKegiatan.jsx
+++ src/DaftarKegiatan.jsx
@@ -1,20 +1,39 @@
 import { Box, Typography } from "@mui/material";
 import TableData from "./component/TableData";
 import ModalForm from "./component/ModalForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DaftarKegiatan() {
+  const [dataKaryawan, setDataKaryawan] = useState([]);
+
+  const fetchDataKaryawan = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/karyawan");
+      setDataKaryawan(response.data ? response.data : []);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataKaryawan();
+  }, []);
+
   return (
     <Box sx={{ margin: "1.5% auto", background: "#fff", width: "96%", paddingBottom: "50px", borderRadius: "10px" }}>
-      <Box display={"flex"} gap={5} padding={3} boxShadow={"0 5px #f7f8fb"}>
-        <Box>
-          <Typography>Nama Karyawan</Typography>
-          <Typography fontWeight={"bold"}>Shyane Pattynama</Typography>
+      {dataKaryawan.map((item) => (
+        <Box key={item.id} display={"flex"} gap={5} padding={2} boxShadow={"0 5px #f7f8fb"}>
+          <Box>
+            <Typography>Nama Karyawan</Typography>
+            <Typography>{item.nama}</Typography>
+          </Box>
+          <Box>
+            <Typography>Rate</Typography>
+            <Typography>{item.rate}</Typography>
+          </Box>
         </Box>
-        <Box>
-          <Typography>Rate</Typography>
-          <Typography fontWeight={"bold"}>Rp.50000/Jam</Typography>
-        </Box>
-      </Box>
+      ))}
       <Box>
         <Box display={"flex"} padding={3} alignItems={"center"} gap={3}>
           <Typography fontWeight={"bold"}>Daftar Kegiatan</Typography>
